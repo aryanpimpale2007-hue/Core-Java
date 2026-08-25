011dd09393be40b552891400d29550b9e051613e
Aryan Pimpale <aryan.pimpale2007@gmail.com>
2026-08-25 21:02:45 +0530

    Add Progressive number pattern

---
+++ PatternPrint.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PatternPrint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter rows: ");
+        int n = sc.nextInt();
+
+        int num = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" "+num);
+                num = num+1;
+            }
+            System.out.println();
+        }
+    }
+}
